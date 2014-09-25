2ea14e300084434b4bc9eb487f0279a53f21625f
Nicholas Hydock <nhydock@gmail.com>
2014-09-25 12:51:22 -0400

    Add mock people for our mock person gateway for testing

--- test/data/gateways/MockPersonGateway.java
+++ test/data/gateways/MockPersonGateway.java
@@ -1,14 +1,33 @@
 package data.gateways;
 
+import java.util.HashMap;
+
 import data.gateway.interfaces.IPersonGateway;
 import data.keys.Key;
+import data.keys.PersonKey;
 import domain.model.Person;
 
 public class MockPersonGateway implements IPersonGateway {
 
+	private static HashMap<Key, Person> mockList = new HashMap<Key, Person>();
+	
+	static
+	{
+		mockList.put(new PersonKey(0), new MockPerson());
+		mockList.put(new PersonKey(1), new MockPerson());
+		mockList.put(new PersonKey(2), new MockPerson());
+		mockList.put(new PersonKey(3), new MockPerson());
+	}
+	
 	@Override
 	public Person find(Key key) {
-		// TODO Auto-generated method stub
+		if (key instanceof PersonKey)
+		{
+			if (mockList.containsKey(key))
+			{
+				return mockList.get(key);
+			}
+		}
 		return null;
 	}
 
@@ -30,4 +49,13 @@ public class MockPersonGateway implements IPersonGateway {
 		
 	}
 
+	
+	/**
+	 * Mock class for representing a person in testing
+	 * @author nhydock
+	 *
+	 */
+	private static class MockPerson extends Person{
+		private MockPerson(){}
+	}
 }
