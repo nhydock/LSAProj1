b86b465aeebf6efe0ae7a1a7a6dd2dfd12197609
Nicholas Hydock <nhydock@gmail.com>
2014-09-25 12:18:41 -0400

    Add base Gateway interface and Keys for fetching through gateways

---
+++ src/data/keys/Key.java
@@ -0,0 +1,12 @@
+package data.keys;
+
+/**
+ * Interface used to define the primary key used in fetching
+ * objects through the gateways
+ * 
+ * @author nhydock
+ */
+public interface Key {
+	@Override
+	public boolean equals(Object key);
+}
--- test/data/gateways/MockPersonGateway.java
+++ test/data/gateways/MockPersonGateway.java
@@ -1,7 +1,33 @@
 package data.gateways;
 
 import data.gateway.interfaces.IPersonGateway;
+import data.keys.Key;
+import domain.model.Person;
 
 public class MockPersonGateway implements IPersonGateway {
+
+	@Override
+	public Person find(Key key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Person object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Person object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void persist(Person object) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
