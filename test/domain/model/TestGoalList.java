99b1b3b8e4eabc04d313dfe43c25e49e3e1c853b
ChrisHersh <ch5968@cs.ship.edu>
2014-09-26 14:08:33 -0400

    Added testInsert for goalList

--- test/domain/model/TestGoalList.java
+++ test/domain/model/TestGoalList.java
@@ -2,6 +2,8 @@ package domain.model;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+
 import org.junit.Test;
 
 public class TestGoalList
@@ -14,6 +16,17 @@ public class TestGoalList
 		assertNotNull(gl.getGoalList());
 		
 		assertEquals(gl.getGoalList().size(), 0);
+	}
+	
+	@Test
+	public void testInsert()
+	{
+		GoalList gl = new GoalList();
+		Goal testG = new Goal(123456789, new Date(123456), new Date(231654897),	new Person(1, "name"));
+		
+		gl.insert(testG);
+		assertEquals(gl.getGoalList().get(0), testG);
+		assertEquals(gl.getGoalList().size(),1);
 	}
 
 }
