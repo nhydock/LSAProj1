3c65bc3b158393924eacc728cfa923f2c75e8594
ChrisHersh <ch5968@cs.ship.edu>
2014-09-26 14:20:20 -0400

    Added testUpdate to goalList tests

--- test/domain/model/TestGoalList.java
+++ test/domain/model/TestGoalList.java
@@ -40,5 +40,19 @@ public class TestGoalList
 		assertEquals(gl.getGoalList().size(),0);
 		
 	}
+	
+	@Test
+	public void testUpdate()
+	{
+		GoalList gl = new GoalList();
+		Goal testG = new Goal(123456789, new Date(123456), new Date(231654897),	new Person(1, "name"));
+		Goal newG = new Goal(85, new Date(2), new Date(5835),	new Person(35, "name"));
+		
+		gl.insert(testG);
+		gl.update(newG, 0);
+		
+		assertEquals(gl.getGoalList().get(0), newG);
+		assertEquals(gl.getGoalList().size(),1);
+	}
 
 }
