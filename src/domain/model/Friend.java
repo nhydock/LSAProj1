730b7391c92af77c99535b2b7ebdcfdee3fb21ae
ChrisHersh <ch5968@cs.ship.edu>
2014-09-30 11:50:05 -0400

    Fixed shit

--- src/domain/model/Friend.java
+++ src/domain/model/Friend.java
@@ -3,11 +3,7 @@ package domain.model;
 public class Friend extends Person {
 
     public Friend(long id, String name) {
-<<<<<<< HEAD
 	super(id, name);
-=======
-    	super(id, name);
->>>>>>> 8dd7f5d90cef095704158691de7a027be0cbabbe
     }
     
 }
--- test/domain/model/TestFriend.java
+++ test/domain/model/TestFriend.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-<<<<<<< HEAD
 public class TestFriend {
 
     @Test
@@ -13,20 +12,5 @@ public class TestFriend {
 	assertEquals(friend.getID(), 1);
 	assertEquals(friend.getName(), "Frank");
     }
-=======
-public class TestFriend
-{
-
-	@Test
-	public void testInitialization()
-	{
-		int id = 0;
-		String name = "Smitty Jaegermanjenssen";
-		Friend friend = new Friend(id, name);
-		assertEquals(friend.getID(), id);
-		assertEquals(friend.getName(), name);
-		
-	}
->>>>>>> 8dd7f5d90cef095704158691de7a027be0cbabbe
 
 }
