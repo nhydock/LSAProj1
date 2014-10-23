54a9a64c4f1fc18bd938be943cef863173232ee3
Nicholas Hydock <nhydock@gmail.com>
2014-10-23 13:46:22 -0400

    Add our mappers to default sessions

--- src/system/Session.java
+++ src/system/Session.java
@@ -4,16 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.HashMap;
 
+import data.gateway.*;
 import data.gateway.interfaces.Gateway;
 import domain.IdentityMap;
 import domain.UnitOfWork;
-import domain.mappers.DataMapper;
-import domain.mappers.UserMapper;
+import domain.mappers.*;
 import domain.model.DomainModelObject;
 import domain.model.User;
 
 public class Session {
-
+    
     Connection connection;
     HashMap<Class<? extends DataMapper<?>>, DataMapper<?>> mappers;
     HashMap<Class<? extends Gateway>, Gateway> gateways;
@@ -29,13 +29,14 @@ public class Session {
         String db = "fitness5"; // ODBC database name
         String host = "lsagroup5.cbzhjl6tpflt.us-east-1.rds.amazonaws.com";
         String user = "lsagroup5";
-        String pass = "lsagroupt5";
+        String pass = "lsagroup5";
         System.out.println("Attempting to open database " + db + "...");
         try {
-            String connectFormat = "jdbc:mysql:%s?user=%s&password=%s";
-            
+            String connectFormat = "jdbc:mysql://%s/%s?user=%s&password=%s";
+            String connectURL = String.format(connectFormat, host, db, user, pass);
+            System.out.println(connectURL);
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(String.format(connectFormat, host, user, pass));
+            connection = DriverManager.getConnection(connectURL);
         } catch (Exception ex) {
             // if not successful, quit
             System.out.println("Cannot open database -- make sure MySQL JDBC is configured properly.");
@@ -43,14 +44,25 @@ public class Session {
         }
         
         mappers = new HashMap<Class<? extends DataMapper<?>>, DataMapper<?>>();
+        gateways = new HashMap<Class<? extends Gateway>, Gateway>();
         identityMaps = new HashMap<Class<? extends DomainModelObject>, IdentityMap<?>>();
         unitOfWork = new UnitOfWork();
         
         addMappers();
+        addGateways();
     }
     
     private void addMappers() {
         mappers.put(UserMapper.class, new UserMapper());
+        mappers.put(FriendsMapper.class, new FriendsMapper());
+        mappers.put(PendingFriendsMapper.class, new PendingFriendsMapper());
+    }
+    
+    private void addGateways() {
+        gateways.put(FriendGateway.class, new FriendGateway());
+        gateways.put(PendingFriendsGateway.class, new PendingFriendsGateway());
+        gateways.put(UserGateway.class, new UserGateway());
+        
     }
 
     private static final ThreadLocal<Session> session = new ThreadLocal<Session>() {
