90dbfc451fa6d74b6d6538a1e89ec0e35d26599c
ChrisHersh <ch5968@cs.ship.edu>
2014-11-08 11:28:41 -0500

    Added more tests for Person, should be at 100% once the tests pass

--- test/domain/model/TestPerson.java
+++ test/domain/model/TestPerson.java
@@ -6,7 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import data.containers.PersonData;
-
+import domain.model.proxies.FriendListProxy;
+import domain.model.proxies.PendingFriendsListProxy;
 import system.Session;
 import system.TestSession;
 
@@ -31,6 +32,18 @@ public class TestPerson {
         assertEquals(person.getPassword(), dataA.password);
         assertEquals(person2.getDisplayName(), dataB.displayName);
         assertNotNull(person.getFriendList());
+    }
+    
+    @Test
+    public void testInitialization2()
+    {
+        Person person = new Person(dataA.name, dataA.password, dataA.displayName);
+        Person person2 = new Person(dataB.name, dataB.password, dataB.displayName);
+        assertEquals(person.getID(), -1);
+        assertEquals(person.getUserName(), dataA.name);
+        assertEquals(person.getPassword(), dataA.password);
+        assertEquals(person2.getDisplayName(), dataB.displayName);
+        assertNull(person.getFriendList());
     }
 
     @Test
@@ -64,33 +77,103 @@ public class TestPerson {
     public void testAcceptFriend() {
     	Person person = new Person(dataA.name, dataA.password, dataA.displayName, dataA.id);
         Person person2 = new Person(dataB.name, dataB.password, dataB.displayName, dataB.id);
+                
+        person.requestFriend(person2);
+        Session.getUnitOfWork().commit();
+        person2.acceptFriendRequest(person);
+        
+        ((FriendListProxy)person.getFriendList()).unload();
+        ((PendingFriendsListProxy)person.getPendingFriends()).unload();
+        
+        assertEquals(1, person.getFriendList().getFriends().size());
+        assertEquals(0, person.getPendingFriends().getAllRequests().size());
+    }
+    
+    @Test 
+    public void testRemoveFriend() {
+        Person person = new Person(dataA.name, dataA.password, dataA.displayName, dataA.id);
+        Person person2 = new Person(dataB.name, dataB.password, dataB.displayName, dataB.id);
+        
+        person.requestFriend(person2);
+        Session.getUnitOfWork().commit();
+        person2.acceptFriendRequest(person);
+        
+        person.removeFriend(person2);
+        
+        assertEquals(0, person.getFriendList().getFriends().size());
+        assertEquals(0, person2.getFriendList().getFriends().size());
+    }
+    
+    @Test
+    public void testRequestFriend() {
+        Person person = new Person(dataA.name, dataA.password, dataA.displayName, dataA.id);
+        Person person2 = new Person(dataB.name, dataB.password, dataB.displayName, dataB.id);
         
         assertEquals(0, person.getFriendList().getFriends().size());
         assertEquals(0, person.getPendingFriends().getAllRequests().size());
-        
+                
         person.requestFriend(person2);
         
         assertEquals(0, person.getFriendList().getFriends().size());
         assertEquals(1, person.getPendingFriends().getAllRequests().size());
         
-        person.acceptFriendRequest(person2);
-        assertEquals(0, person.getPendingFriends().getAllRequests().size());
-        assertEquals(1, person.getFriendList().getFriends().size());
+        assertEquals(0, person2.getFriendList().getFriends().size());
+        assertEquals(1, person2.getPendingFriends().getAllRequests().size());
+    }
+    
+    @Test
+    public void testDeclineFriendRequest() {
+        Person person = new Person(dataA.name, dataA.password, dataA.displayName, dataA.id);
+        Person person2 = new Person(dataB.name, dataB.password, dataB.displayName, dataB.id);
+                
+        person.requestFriend(person2);
+        Session.getUnitOfWork().commit();
+        person2.declineFriendRequest(person);
         
+        ((FriendListProxy)person.getFriendList()).unload(); 
+        ((PendingFriendsListProxy)person.getPendingFriends()).unload();
+        
+        assertEquals(0, person.getFriendList().getFriends().size());
+        assertEquals(0, person.getPendingFriends().getAllRequests().size());
+        
+        assertEquals(0, person2.getFriendList().getFriends().size());
+        assertEquals(0, person2.getPendingFriends().getAllRequests().size());
     }
     
     @Test
-    public void testRemoveFriend() {
-    	fail("Not yet tested");
+    public void testSetDisplayName()
+    {
+        Person person = new Person(dataA.name, dataA.password, dataA.displayName, dataA.id);
+        assertEquals(person.getDisplayName(), dataA.displayName);
+        
+        String newName = "New name";
+        
+        person.setDisplayName(newName);
+        assertEquals(person.getDisplayName(), newName);
     }
     
     @Test
-    public void testRequestFriend() {
-    	fail("Not yet tested");
+    public void testGetFriendsList()
+    {
+        Person person = new Person(dataA.name, dataA.password, dataA.displayName, dataA.id);
+        Person person2 = new Person(dataB.name, dataB.password, dataB.displayName, dataB.id);
+        
+        assertEquals(person.getFriends().size(), 0);
+        assertEquals(person.getFriendList().getFriends().size(), 0);
+        
+        person.requestFriend(person2);
+        Session.getUnitOfWork().commit();
+        person2.acceptFriendRequest(person);
+        
+        assertEquals(person.getFriends().size(), 1);
+        assertEquals(person.getFriendList().getFriends().size(), 1);
     }
     
     @Test
-    public void testDenyFriendRequest() {
-    	fail("Not yet tested");
+    public void testToString()
+    {
+        Person person = new Person(dataA.name, dataA.password, dataA.displayName, dataA.id);
+        
+        assertEquals(person.toString(), dataA.name + ":" + dataA.password + ":" + dataA.displayName);
     }
 }
